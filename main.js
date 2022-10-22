9bd72addaaaaeec718dca219b8a4616f17ff42b9
BlackWolf-117S <betoislas11759@gmail.com>
2022-10-22 13:39:36 -0500

    Se agrego el menú hamburguesa para móviles

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const Cuenta = document.querySelector('#cuenta');
 const NavCuenta = document.querySelector('#nav-cuenta');
 
+const NavIconoMenuAmburgesa = document.querySelector('#nav-icono-menu-amburgesa');
+const NavMenuMovile = document.querySelector('#nav-menu-movile');
+
 Cuenta.addEventListener('click', (event) => {
 	NavCuenta.classList.toggle('inactive');
 	console.log(event);
+});
+NavIconoMenuAmburgesa.addEventListener('click', (event) => {
+	NavMenuMovile.classList.toggle('inactive');
 });
